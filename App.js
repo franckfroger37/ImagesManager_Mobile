ead3999762390040bcc948827629c5e798cc736b
franckfroger37 <franck.froger.37@gmail.com>
2026-03-22 20:01:08 +0100

    Fix emoji corruption + set Images Manager V2

--- App.js
+++ App.js
@@ -29,7 +29,7 @@ export default function App() {
           <Stack.Screen
             name="Home"
             component={HomeScreen}
-            options={{ title: 'ð· Images Manager V2', headerLeft: () => null }}
+            options={{ title: '📷 Images Manager V2', headerLeft: () => null }}
           />
           <Stack.Screen
             name="Crop"
